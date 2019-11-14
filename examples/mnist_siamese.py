ecf14a8f8f42ce5b80598b0b6b599f45eeb71eaf
t.takigami <takigami-t@ise-ics.co.jp>
2019-11-14 17:47:42 +0900

    siamese の勉強用コメントの追加

--- examples/mnist_siamese.py
+++ examples/mnist_siamese.py
@@ -1,3 +1,5 @@
+# %%
+
 '''Trains a Siamese MLP on pairs of digits from the MNIST dataset.
 
 It follows Hadsell-et-al.'06 [1] by computing the Euclidean distance on the
@@ -22,6 +24,7 @@ from keras.models import Model
 from keras.layers import Input, Flatten, Dense, Dropout, Lambda
 from keras.optimizers import RMSprop
 from keras import backend as K
+from keras.utils import plot_model
 
 num_classes = 10
 epochs = 20
@@ -51,18 +54,25 @@ def contrastive_loss(y_true, y_pred):
 def create_pairs(x, digit_indices):
     '''Positive and negative pair creation.
     Alternates between positive and negative pairs.
+    
+    Positive と negative のペアを作成する
     '''
     pairs = []
     labels = []
     n = min([len(digit_indices[d]) for d in range(num_classes)]) - 1
     for d in range(num_classes):
         for i in range(n):
+            # z1, z2 はインデックス
             z1, z2 = digit_indices[d][i], digit_indices[d][i + 1]
+            # x は画像データ
+            # ここでポジティブペアを作成する
             pairs += [[x[z1], x[z2]]]
             inc = random.randrange(1, num_classes)
             dn = (d + inc) % num_classes
             z1, z2 = digit_indices[d][i], digit_indices[dn][i]
+            # ここでネガティブペアを作成している
             pairs += [[x[z1], x[z2]]]
+            # ポジティブペアを 1, ネガティブペアを 0 にラベリングしている
             labels += [1, 0]
     return np.array(pairs), np.array(labels)
 
@@ -94,20 +104,33 @@ def accuracy(y_true, y_pred):
 
 
 # the data, split between train and test sets
+## MNIST 手書き数字データベースをロード
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
+## 60,000枚の28x28の白黒データなので imput_shape == [28, 28]
 input_shape = x_train.shape[1:]
 
+# %%
 # create training+test positive and negative pairs
+## このリスト内包表現でインデックスに対応した y_train のデータのインデックスを格納した配列を取得する
+## y_train が [5 0 4 ... 5 6 8]のようなデータの時
+## digit_indices は
+## [[1 ...], # 0
+## ...
+## [0 ... N-2], # 5
+## ...
+## [...]] # 9
+## のような二次元配列が取得できる
 digit_indices = [np.where(y_train == i)[0] for i in range(num_classes)]
 tr_pairs, tr_y = create_pairs(x_train, digit_indices)
 
 digit_indices = [np.where(y_test == i)[0] for i in range(num_classes)]
 te_pairs, te_y = create_pairs(x_test, digit_indices)
 
+# %%
 # network definition
 base_network = create_base_network(input_shape)
 
@@ -141,3 +164,7 @@ te_acc = compute_accuracy(te_y, y_pred)
 
 print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
 print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
+
+
+# %%
+plot_model(model, to_file='model.png', show_shapes=True)
